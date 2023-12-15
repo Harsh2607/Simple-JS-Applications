c67775f260a55dcfcdef8895bda93d2f66a38075
HG <harshgarg2607@gmail.com>
2023-12-15 11:24:22 +0530

    sample menu design

---
+++ Project 1 - Sample Menu Design/P1_javascript.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("menu")
+
+Array.from(document.getElementByClassName("menu-item")).forEach((item,index) => {
+	item.onmouseover = () => {
+		menu.dataset.activeIndex = index;
+	}
+});
