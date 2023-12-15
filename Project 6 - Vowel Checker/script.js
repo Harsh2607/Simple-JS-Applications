00fe91ac393090c18e3d6fcab2baf337567cda89
HARSH GARG <54450210+Harsh2607@users.noreply.github.com>
2023-12-15 14:56:47 +0530

    new method added

--- Project 6 - Vowel Checker/script.js
+++ Project 6 - Vowel Checker/script.js
@@ -1,3 +1,5 @@
+// method 1
+
 function countVowels() {
     const text = document.getElementById('input-text').value.toLowerCase();
     const vowelCount = (text.match(/[aeiou]/g) || []).length; // Using regex to count vowels
@@ -7,3 +9,27 @@ function countVowels() {
 
 const countBtn = document.getElementById('count-btn');
 countBtn.addEventListener('click', countVowels);
+
+// method 2
+
+// function isVowel(ch) {
+//     var vowels = ["a", "e", "i", "o", "u"];
+//     return vowels.includes(ch);
+// }
+
+// function countVowels() {
+//     var text = document.getElementById("input-text").value;
+//     var vowelCount = 0;
+//     text = text.toLowerCase();
+//     for (var i = 0; i < text.length; i++) {
+//         var ch = text.charAt(i);
+//         if (isVowel(ch)) {
+//             vowelCount++;
+//         }
+//     }
+//     var result = document.getElementById('result');
+//     result.textContent = "Total vowels: " + vowelCount;
+// }
+
+// const countVowelsBtn = document.getElementById('count-btn');
+// countVowelsBtn.addEventListener('click', countVowels);
